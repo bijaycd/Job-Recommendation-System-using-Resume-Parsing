c16f1310d2b5a08f4b6839178208da20272aa846
Bijay Chandra Das <bdas64316@gmail.com>
2025-03-08 01:45:25 +0530

    requirements.txt added

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ import pickle
 app = Flask(__name__)
 
 # Load models===========================================================================================================
-rf_classifier_categorization = pickle.load(open('models/rf_classifier_categorization.pkl', 'rb'))
-tfidf_vectorizer_categorization = pickle.load(open('models/tfidf_vectorizer_categorization.pkl', 'rb'))
-rf_classifier_job_recommendation = pickle.load(open('models/rf_classifier_job_recommendation.pkl', 'rb'))
-tfidf_vectorizer_job_recommendation = pickle.load(open('models/tfidf_vectorizer_job_recommendation.pkl', 'rb'))
+
+rf_classifier_categorization = pickle.load(open('C:\Projects\Job-Recommendation-System-using-Resume-Parsing\Models/rf_classifier_categorization.pkl', 'rb'))
+tfidf_vectorizer_categorization = pickle.load(open('C:\Projects\Job-Recommendation-System-using-Resume-Parsing\Models/tfidf_vectorizer_categorization.pkl', 'rb'))
+# rf_classifier_job_recommendation = pickle.load(open('models/rf_classifier_job_recommendation.pkl', 'rb'))
+# tfidf_vectorizer_job_recommendation = pickle.load(open('models/tfidf_vectorizer_job_recommendation.pkl', 'rb'))
 
 # Clean resume==========================================================================================================
 def cleanResume(txt):
@@ -230,7 +231,7 @@ def extract_name_from_resume(text):
 @app.route('/')
 def resume():
     # Provide a simple UI to upload a resume
-    return render_template("resume.html")
+    return render_template("index.html")
 
 @app.route('/pred', methods=['POST'])
 def pred():
@@ -243,7 +244,7 @@ def pred():
         elif filename.endswith('.txt'):
             text = file.read().decode('utf-8')
         else:
-            return render_template('resume.html', message="Invalid file format. Please upload a PDF or TXT file.")
+            return render_template('index.html', message="Invalid file format. Please upload a PDF or TXT file.")
 
         predicted_category = predict_category(text)
         recommended_job = job_recommendation(text)
@@ -254,10 +255,10 @@ def pred():
         extracted_education = extract_education_from_resume(text)
         name = extract_name_from_resume(text)
 
-        return render_template('resume.html', predicted_category=predicted_category,recommended_job=recommended_job,
+        return render_template('index.html', predicted_category=predicted_category,recommended_job=recommended_job,
                                phone=phone,name=name,email=email,extracted_skills=extracted_skills,extracted_education=extracted_education)
     else:
-        return render_template("resume.html", message="No resume file uploaded.")
+        return render_template("index.html", message="No resume file uploaded.")
 
 if __name__ == '__main__':
     app.run(debug=True)
